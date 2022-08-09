81544a0defa53ec116008580d4039484506d0395
Jan Szejko <janek37@gmail.com>
2022-08-09 02:10:00 +0200

    Add index and current id

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,11 +9,15 @@ import {InputTree} from "../InputTree";
 
 const MAX_LEVEL = 5;
 
-class App extends React.Component<{}, {data: InputTree | undefined}> {
+type Index = {[key: number]: InputTree};
+
+class App extends React.Component<{}, {data: InputTree | undefined; currentId: number, index: Index}> {
   constructor(props: {}) {
     super(props);
     this.state = {
       data: undefined,
+      currentId: 0,
+      index: {},
     };
   }
 
@@ -21,15 +25,29 @@ class App extends React.Component<{}, {data: InputTree | undefined}> {
     axios.get(`/export.json`)
       .then(res => {
         const data = res.data;
-        this.setState({ data });
+        const index = this.buildIndex(data);
+        this.setState({ data, index });
       });
+  }
+
+  buildIndex(data: InputTree): Index {
+    let index: Index = {};
+    if ('children' in data) {
+      if (data.children.length > 1) {
+        index[data.id] = data;
+      }
+      for (const child of data.children) {
+        Object.assign(index, this.buildIndex(child));
+      }
+    }
+    return index;
   }
 
   render() {
     if (!this.state.data) {
       return <div></div>
     }
-    const data = trimData(this.state.data, MAX_LEVEL) as TreeParent;
+    const data = trimData(this.state.index[this.state.currentId], MAX_LEVEL) as TreeParent;
     const graph = makeGraph(data);
     return (
       <div className="App">
