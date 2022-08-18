a5514b447206d0e477a005cd078f620dd77743c4
Jan Szejko <janek37@gmail.com>
2022-08-18 22:27:01 +0200

    Add prefix to extinct.svg

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,7 +9,7 @@ function Image(props: {leaf: Leaf, positioner: positioner, onClick: (nodeId: Tre
   const {x, y} = props.positioner.getImageCoordinates(leaf.position);
   return <g className='image' onClick={() => {props.onClick(leaf.value)}}>
     <image href={process.env.PUBLIC_URL + leaf.value.image.local_thumbnail_url} width={size} height={size} x={x - size/2} y={y - size/2}/>
-    {leaf.value.extinct ? <image href='/extinct.svg' width={size/4} height={size/4} x={x - size/2} y={y - size/2} /> : ''}
+    {leaf.value.extinct ? <image href={`${process.env.PUBLIC_URL}/extinct.svg`} width={size/4} height={size/4} x={x - size/2} y={y - size/2} /> : ''}
     <text x={x + size/2} y={y + size/2} className='num' textAnchor='end'>{props.leaf.value.speciesCount}</text>
     <title>{leaf.value.name}</title>
   </g>;
